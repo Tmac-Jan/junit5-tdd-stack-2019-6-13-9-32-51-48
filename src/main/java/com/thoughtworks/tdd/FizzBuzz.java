8bb6907b94216851a0fe5eeaaf9b2e40a5c3bc17
zhanggi3 <gio.zhang@oocl.com>
2019-07-12 13:53:29 +0800

    [eric li]:[feat:implement should_get_FizzBuzzWhizz_when_given_105]

--- src/main/java/com/thoughtworks/tdd/FizzBuzz.java
+++ src/main/java/com/thoughtworks/tdd/FizzBuzz.java
@@ -16,13 +16,28 @@ public class FizzBuzz {
     this.num = num;
   }
 
-  public String say(int sum) {
-    boolean mod3 = sum % 3 == 0 ? true : false;
-    boolean mod5 = sum % 5 == 0 ? true : false;
-    boolean mod7 = sum % 7 == 0 ? true : false;
-    return (mod3)
-        ? (mod5 ? (mod7 ? "FizzBuzzWhizz" : "FizzBuzz")
-        : mod7?"FizzWhizz":"Fizz")
-        : (mod7 ? "Whizz" : sum + "");
+  //  public String say(int sum) {
+//    boolean mod3 = sum % 3 == 0 ? true : false;
+//    boolean mod5 = sum % 5 == 0 ? true : false;
+//    boolean mod7 = sum % 7 == 0 ? true : false;
+//    return (mod3)
+//        ? (mod5 ? (mod7 ? "FizzBuzzWhizz" : "FizzBuzz")
+//        : mod7?"FizzWhizz":"Fizz")
+//        : (mod7 ? "Whizz" : sum + "");
+//  }
+  public String say(int num) {
+    String result = "";
+    if (num % 3 == 0 || num % 5 == 0 || num % 7 == 0) {
+      if (num % 3 == 0) {
+        result += "Fizz";
+      }
+      if (num % 5 == 0) {
+        result += "Buzz";
+      }
+      if (num % 7 == 0) {
+        result += "Whizz";
+      }
+    }
+    return result;
   }
 }
